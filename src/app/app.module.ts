fb768e59be55c0297546cb77c656551a0938fae5
cmlozanos <carlosmlozanos@gmail.com>
2021-02-23 17:46:28 +0100

    añadir componente de crear usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { UserListComponent } from './components/users/user-list/user-list.component';
 import { CarListComponent } from './components/cars/car-list/car-list.component';
 import { DogListComponent } from './components/dogs/dog-list/dog-list.component';
+import { AddUserComponent } from './components/users/add-user/add-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserListComponent,
     CarListComponent,
-    DogListComponent
+    DogListComponent,
+    AddUserComponent
   ],
   imports: [
     BrowserModule,
