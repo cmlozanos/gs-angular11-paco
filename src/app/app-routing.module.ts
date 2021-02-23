b4327745ea687fa79599256a1063238e765370f9
cmlozanos <carlosmlozanos@gmail.com>
2021-02-23 17:28:51 +0100

    dogs component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarListComponent } from './components/cars/car-list/car-list.component';
+import { DogListComponent } from './components/dogs/dog-list/dog-list.component';
 import { UserListComponent } from './components/users/user-list/user-list.component';
 
 const routes: Routes = [
   { path: 'cars', component: CarListComponent },
+  { path: 'dogs' , component: DogListComponent},
   { path: '',      component: UserListComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './components/users/user-list/user-list.component';
 import { CarListComponent } from './components/cars/car-list/car-list.component';
+import { DogListComponent } from './components/dogs/dog-list/dog-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserListComponent,
-    CarListComponent
+    CarListComponent,
+    DogListComponent
   ],
   imports: [
     BrowserModule,
