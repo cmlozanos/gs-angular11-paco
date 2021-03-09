1439d97431fbe11816df0974e7e38f85917074ae
cmlozanos <carlosmlozanos@gmail.com>
2021-03-09 17:02:27 +0100

    add required and optional fields

--- src/app/shared/models/user.ts
+++ src/app/shared/models/user.ts
@@ -1,3 +1,3 @@
 export class User {
-    constructor(public name: string, public lastname: string, public salary: number){};
+    constructor(public name: string, public lastname?: string, public salary?: number){};
 }
